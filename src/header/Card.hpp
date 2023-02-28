b0307627efaa0af6c52d05abbd2419c45c3b9deb
alifioditya <alifioditya@gmail.com>
2023-02-28 11:39:16 +0700

    Added card starter pack

--- src/header/Card.hpp
+++ src/header/Card.hpp
@@ -3,55 +3,44 @@
 #define CARD_HPP
 
 #include <string>
+#include "../enums/CardTypes.hpp"
 
 using std::string;
 
 class Card {
 private:
-    string suit;
-    string rank;
+    Suit suit;
+    Rank rank;
 
 public:
     // Default constructor
-    Card() : suit(""), rank("") {}
+    Card();
 
     // Constructor with parameters
-    Card(string s, string r) : suit(s), rank(r) {}
+    Card(Suit s, Rank r);
 
     // Copy constructor
-    Card(const Card& other) : suit(other.suit), rank(other.rank) {}
+    Card(const Card& other);
 
     // Assignment operator
-    Card& operator=(const Card& other) {
-        if (this != &other) {
-            suit = other.suit;
-            rank = other.rank;
-        }
-        return *this;
-    }
+    Card& operator=(const Card&);
 
     // Equality operator
-    bool operator==(const Card& other) const {
-        return suit == other.suit && rank == other.rank;
-    }
+    bool operator==(const Card&) const;
 
     // Getters for suit and rank
-    string getSuit() const {
-        return suit;
-    }
+    Suit getSuit() const;
 
-    string getRank() const {
-        return rank;
-    }
+    Rank getRank() const;
 
     // Setters for suit and rank
-    void setSuit(string s) {
-        suit = s;
-    }
+    void setSuit(Suit s);
 
-    void setRank(string r) {
-        rank = r;
-    }
+    void setRank(Rank r);
+
+    // Get string representation of suit and rank
+    string getSuitString() const;
+    string getRankString() const;
 };
 
 #endif

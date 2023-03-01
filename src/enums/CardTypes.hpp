8514ece3b4bb1ef81e52bad7b1d092c8bbe3beae
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-03-01 14:52:46 +0700

    fix: card headers

--- src/enums/CardTypes.hpp
+++ src/enums/CardTypes.hpp
@@ -2,15 +2,10 @@
 #ifndef CARDENUMS_HPP
 #define CARDENUMS_HPP
 
-enum Suit {
-    CLUBS = 0,
-    DIAMONDS = 1,
-    HEARTS = 2,
-    SPADES = 3
-};
+enum Color { GREEN = 0, BLUE = 1, YELLOW = 2, RED = 3 };
 
 enum Rank {
-    ACE = 1,
+    ONE = 1,
     TWO = 2,
     THREE = 3,
     FOUR = 4,
@@ -20,9 +15,9 @@ enum Rank {
     EIGHT = 8,
     NINE = 9,
     TEN = 10,
-    JACK = 11,
-    QUEEN = 12,
-    KING = 13
+    ELEVEN = 11,
+    TWELVE = 12,
+    THIRTEEN = 13
 };
 
 #endif
---
+++ src/header/Cards/Combination.hpp
@@ -0,0 +1,18 @@
+#ifndef COMBINATION_HPP
+#define COMBINATION_HPP
+
+#include ""
+#include "CardInterface.hpp"
+
+// KELAS SANGAT AMBIGU
+// DO NO USE
+// TP WAJIB DI SPEK
+// SEMENTARA NYIMPEN KALKULASI KOMBINASI DULU
+class Combination : public CardInterface {
+   private:
+   public:
+    Combination();
+    ~Combination();
+};
+
+#endif

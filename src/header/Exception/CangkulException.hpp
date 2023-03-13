41c97d599595ff298d8fca9514b603ac3e1ef3f1
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2023-03-13 23:33:56 +0700

    add: uncomplete cangkulgamemanager

---
+++ src/header/Exception/CangkulException.hpp
@@ -0,0 +1,22 @@
+#ifndef CANGKULEXCEPTION_HPP
+#define CANGKULEXCEPTION_HPP
+
+#include <exception>
+
+using std::exception;
+
+class NotEnoughPlayerException : public exception {
+public:
+    const char* what() const throw() {
+        return "Jumlah pemain terlalu sedikit";
+    }
+};
+
+class TooManyPlayerException : public exception {
+public:
+    const char* what() const throw() {
+        return "Jumlah pemain terlalu banyak";
+    }
+};
+
+#endif

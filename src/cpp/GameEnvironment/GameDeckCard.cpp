8554c0b218302cc0af4a4849976af68ac6b058f1
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2023-03-13 09:05:25 +0700

    fix: minor-bug

--- src/cpp/GameEnvironment/GameDeckCard.cpp
+++ src/cpp/GameEnvironment/GameDeckCard.cpp
@@ -1,22 +1,20 @@
 #include "../../header/GameEnvironment/GameDeckCard.hpp"
 #include "../../header/GameEnvironment/DeckCard.hpp"
 #include <algorithm>
+
 using std::random_shuffle;
 
 GameDeckCard::GameDeckCard() {
     defaultConfig();
 }
 
-GameDeckCard::GameDeckCard(const vector<Card>& cards): DeckCard<Card>(cards) {
-}
+GameDeckCard::GameDeckCard(const vector<Card>& cards): DeckCard<Card>(cards) {}
 
 void GameDeckCard::defaultConfig() {
     vector<Card> tempCards;
 
-    for (int i = 0; i < 4; i++)
-    {
-        for(int j = 1; j <= 13; i++)
-        {
+    for (int i = 0; i < 4; i++) {
+        for(int j = 1; j <= 13; j++) {
             tempCards.push_back(Card((Color)i, (Rank)j));
         }
     }
